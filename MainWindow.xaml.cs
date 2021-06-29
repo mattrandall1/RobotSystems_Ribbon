7556ef0c49075f772d8d50c2063e0d824e7cfe0b
Matt Randall <c3303493@uon.edu.au>
2021-06-29 15:33:28 +1000

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using System.Windows.Controls.Ribbon;
+
+namespace RobotSystems_Ribbon
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : RibbonWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            // Maximises Screen
+            //Application.Current.MainWindow.WindowState = WindowState.Maximized;
+        }
+    }
+}
